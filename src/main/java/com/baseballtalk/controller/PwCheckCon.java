2d2152b2620b9dc7cfdc955916dba0bebd2ca94d
ksks31 <106016609+ksks31@users.noreply.github.com>
2024-06-17 15:34:45 +0900

    test 커밋

--- src/main/java/com/baseballtalk/controller/PwCheckCon.java
+++ src/main/java/com/baseballtalk/controller/PwCheckCon.java
@@ -5,42 +5,38 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.baseballtalk.model.MemberDAO;
+import com.baseballtalk.model.MemberDTO;
 
+@WebServlet("/PwCheckCon")
+public class PwCheckCon extends HttpServlet {
 
-public class PwCheckCon{
-
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String inputPw1 = request.getParameter("???");
-		String inputPw2 = request.getParameter("???");
-		
-		System.out.println(inputPw1);
-		System.out.println(inputPw2);
-	
-		boolean result = new MemberDAO().pwCheck(inputPw1, inputPw2);
-		
-		System.out.println(result);
-		
-		if(result) {
-			// 비밀번호가 일치합니다.
-		}
-		else {
-			// 비밀번호가 일치하지 않습니다.
-		}
-		
-		PrintWriter out;
-		try {
-			out = response.getWriter();
-			out.print(result);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      HttpSession session = request.getSession();
+      request.setCharacterEncoding("UTF-8");
+      String pw = request.getParameter("pw");
+      MemberDTO login_member = (MemberDTO)session.getAttribute("login_member");
+      
+      boolean result = true;
+      if(pw.equals(login_member.getMem_pw())) {
+         result = true;
+      }else {
+         result = false;
+      }
+      PrintWriter out;
+      try {
+         out = response.getWriter();
+         out.print(result);
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
 
 }
+
