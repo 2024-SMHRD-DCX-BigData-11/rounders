dbcaab46334b70cf7159de136352f3e0d5b61521
seungju1212 <seungju1212@daum.net>
2024-06-17 12:22:03 +0900

    커밋

--- src/main/java/testVoteTime.java
+++ src/main/java/testVoteTime.java
@@ -1,5 +1,7 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.baseballtalk.model.MatchDAO;
@@ -7,7 +9,19 @@ import com.baseballtalk.model.MatchDAO;
 public class testVoteTime {
 	public static void main(String[] args) throws ParseException {
 		String time = new MatchDAO().match_time(1);
+		
+		if(time.contains("종료")) {
+			System.out.println("경기종료");
+		}
+		else if(time.contains("취소")) {
+			System.out.println("경기취소");
+		}else if (time.contains("회")) {
+			System.out.println("경기중");
+		}else {
 		Date today = new Date();
+		Calendar now = Calendar.getInstance();
+		System.out.println(time);
+		time = now.get(Calendar.YEAR) + "-" +  time;
 		System.out.println(time);
 		SimpleDateFormat fo = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		Date date = fo.parse(time);
@@ -20,6 +34,6 @@ public class testVoteTime {
 		}else {
 			System.out.println("투표가능");
 		}
-		
+		}
 	}
 }
