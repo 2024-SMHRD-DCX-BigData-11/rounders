79238a667d8412d756b2e549290f09ead45c081a
ksks31 <106016609+ksks31@users.noreply.github.com>
2024-06-18 20:22:05 +0900

    문제되는 것 커밋

--- src/main/java/com/baseballtalk/model/PitcherStatDTO.java
+++ src/main/java/com/baseballtalk/model/PitcherStatDTO.java
@@ -12,22 +12,23 @@ import lombok.ToString;
 @Setter
 @ToString
 
-public class PltcherStatDTO {
+public class PitcherStatDTO {
+	
 	private int pitcher_idx;
-	private int k_per_9;
-	private int b_per_9;
-	private int hr_per_9;
-	private int babip;
-	private int fip;
-	private int fip_war;
-	private int ra9_war;
+	private float k_per_9;
+	private float b_per_9;
+	private float hr_per_9;
+	private float babip;
+	private float fip;
+	private float fip_war;
+	private float ra9_war;
 	private int game_played;
 	private int gs;
 	private int player_win;
 	private int player_lose;
 	private int saves;
 	private int hold;
-	private int ip;
-	private int era;
+	private float ip;
+	private float era;
 	
 }
